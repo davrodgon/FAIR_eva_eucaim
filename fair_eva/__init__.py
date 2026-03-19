e0397d7abf093ea3c55bba45358ad75a7c503f84
Ubuntu <davrodgon@users.noreply.github.com>
2026-03-19 07:58:43 +0000

    Ignore __pycache__ everywhere. Trying to make the app executable by external ASGI server

--- fair_eva/__init__.py
+++ fair_eva/__init__.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
+import pkgutil
+__path__ = pkgutil.extend_path(__path__, __name__)
 
 import connexion
 from connexion.resolver import RestyResolver
 
+# Extracted app for global visibility, allow external/production server access
+app = connexion.FlaskApp(__name__)
+app.add_api(
+    "fair-api.yaml",
+    arguments={"title": "FAIR evaluator"},
+    resolver=RestyResolver("fair_eva.api"),
+)
+
+application = app.app
 
 def main():
-    app = connexion.FlaskApp(__name__)
-    app.add_api(
-        "fair-api.yaml",
-        arguments={"title": "FAIR evaluator"},
-        resolver=RestyResolver("fair_eva.api"),
-    )
-    app.run(port=9090)
+    app.run(port=9090) # Hardcoded port, to be changed, although only used in direct execution
+
+if __name__ == "__main__":
+    main()
